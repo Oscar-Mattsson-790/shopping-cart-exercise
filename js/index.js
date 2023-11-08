6a79947934e91009d0b353c1b8a862cdc407e5d6
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-11-08 12:03:35 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,32 +1,58 @@
-//Variables to use and it's a mix of global and local variables
+//Variables to use
 let shoppingCart = [];
-let products = document.getElementsByTagName("button");
-let cartProducts = "";
+let products = document.querySelectorAll(".card .button");
 
-//This code gets whatever is inside data-product in the HTML
-let product = event.target.parentNode.getAttribute("data-product");
-cartProducts +=
-  '<li><span class="product-title">Titel: </span>' + shoppingCart[i] + "</li>";
-shoppingCart.push(product);
+// Function to check if product is already in the cart
+function isProductInCart(product) {
+  return shoppingCart.includes(product);
+}
 
-//
-document.getElementById("productsInCart").innerHTML = shoppingCart.length;
-document.getElementById("cart").classList.toggle("hide");
-document.getElementById("products").innerHTML = cartProducts;
-products[i].addEventListener("click", function (event) {});
-document.getElementById("open-cart").addEventListener("click", function () {});
+// Event listeners for 'Add to cart' buttons
+for (let i = 0; i < products.length; i++) {
+  products[i].addEventListener("click", function (event) {
+    let product = event.target.closest(".card").getAttribute("data-product");
 
-//Function declarations, add code inside {}
-function updateCart() {}
-function listProductsInCart() {}
+    if (!isProductInCart(product)) {
+      shoppingCart.push(product);
+      updateCart();
+    } else {
+      alert("This book is already added tp the card.");
+    }
+  });
+}
 
-//For-loops of two arrays
-for (let i = 0; i < shoppingCart.length; i++) {}
-for (let i = 0; i < products.length; i++) {}
+// Function to update the cart display
+function updateCart() {
+  document.getElementById("productsInCart").innerHTML = shoppingCart.length;
+  listProductsInCart();
+}
 
-//Function calls and there should be two listProductsInCart()
-updateCart();
-listProductsInCart();
-listProductsInCart();
+// Function to list products in the cart and add a "Remove" button
+function listProductsInCart() {
+  let cartProducts = "";
+  for (let i = 0; i < shoppingCart.length; i++) {
+    cartProducts += `<li>
+      <span class="product-title">Title: </span>${shoppingCart[i]}
+      <button class="remove-button" data-index="${i}">Remove product</button> 
+    </li>`; // Fixed missing opening quote for class and added quotes around ${i}
+  }
+  document.getElementById("products").innerHTML = cartProducts;
+  addRemoveButtonListeners();
+}
 
-//test
+// Add event listeners to the "Remove" buttons after the list update
+function addRemoveButtonListeners() {
+  let removeButtons = document.querySelectorAll(".remove-button");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      let index = parseInt(event.target.getAttribute("data-index"));
+      shoppingCart.splice(index, 1); // Remove the item from the cart
+      updateCart(); // Update the cart display
+    });
+  });
+}
+
+// Toggle cart visibility
+document.getElementById("open-cart").addEventListener("click", function () {
+  document.getElementById("cart").classList.toggle("hide");
+});
